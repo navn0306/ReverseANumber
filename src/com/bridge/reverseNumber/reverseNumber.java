b819a46adc2d550393f351eab10382d0442e7f12
Navneet <navn0306@gmail.com>
2021-11-29 15:54:20 +0530

    ADD: Added the welcome message for program

---
+++ src/com/bridge/reverseNumber/reverseNumber.java
@@ -0,0 +1,7 @@
+package com.bridge.reverseNumber;
+
+public class reverseNumber {
+    public static void main(String[] args) {
+        System.out.println("\nWelcome, this program reverses a given number.");
+    }
+}
